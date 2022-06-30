b2e3c52f571e6c7ad9c449b89d89bd1f5b7705f3
x417v <ilyakinskiy@gmail.com>
2022-06-30 23:31:01 +0300

    second commit - html+css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
 function App() {
   return (
-    <div className="wrapper">
-      
+    <div className="wrapper clear">
+      <header className="d-flex justify-between">
+        <div className="d-flex align-center">
+        <img width={40} height={40} src="/img/logo.png"/>
+        <div className="headerInfo">
+          <h3>React Sneakers</h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+          <img width={20} height={15} src="/img/cart.svg"/>
+            <span>1205 грн.</span>
+          </li>
+          <li>
+          <img width={20} height={20} src="/img/user.svg"/>
+          </li>
+        </ul>
+      </header>
+       <div class="content">
+        <h1>Все кроссовки</h1>
+        ....
+       </div>
     </div>
   );
 }
